5450daba36a725446b824c45c34115040008d609
Siddhant <sidpawbake15@gmail.com>
2025-07-22 16:06:09 +0530

    Optimal Soln added

--- Largest subarray with 0 sum.cpp
+++ Largest subarray with 0 sum.cpp
@@ -17,3 +17,24 @@ class Solution {
         return len;
     }
 };
+
+A2:Optimal
+class Solution {
+  public:
+    int maxLength(vector<int>& arr) {
+        // code here
+        unordered_map<int,int>map;
+        int sum=0;
+        map[0]=-1;
+        int len=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+            if(map.find(sum)!=map.end()){
+                len=max(len,i-map[sum]);
+                continue;
+            }
+            map[sum]=i;
+        }
+        return len;
+    }
+};
