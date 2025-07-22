18aae6fd7871e59804b67745ce0afff8c46d743d
Siddhant <sidpawbake15@gmail.com>
2025-07-22 12:59:37 +0530

    4Sum Added!

---
+++ #18_4Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int size = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < size; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < size; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1, l = size - 1;
+                while (k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum < target) {
+                        k++;
+                    } else if (sum > target) {
+                        l--;
+                    } else {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while (k < l && nums[k] == nums[k + 1]) k++;
+                        while (k < l && nums[l] == nums[l - 1]) l--;
+                        k++;
+                        l--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
