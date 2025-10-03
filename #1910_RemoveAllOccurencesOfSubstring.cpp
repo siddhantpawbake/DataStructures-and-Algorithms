5137f35cb687a1503967251c10ddd72e7efa71e4
Siddhant <sidpawbake15@gmail.com>
2025-10-03 14:16:09 +0530

    Added Java soln

--- #1910_RemoveAllOccurencesOfSubstring.cpp
+++ #1910_RemoveAllOccurencesOfSubstring.cpp
@@ -9,3 +9,13 @@ public:
         return s;
     }
 };
+
+java:
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        while(s.contains(part)){
+            s=s.substring(0,s.indexOf(part))+s.substring(s.indexOf(part)+part.length());
+        }
+        return s;
+    }
+}
