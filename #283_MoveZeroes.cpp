5320982046492205941e93e5fdb4a8d1dc25adfe
Siddhant <sidpawbake15@gmail.com>
2025-11-12 12:51:56 +0530

    Added one more approach!

--- #283_MoveZeroes.cpp
+++ #283_MoveZeroes.cpp
@@ -43,3 +43,16 @@ public:
             }
     }
 };};
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+         int index=0;
+         for(int i=0;i<arr.size();i++){   
+            if(arr[i]!=0){
+                swap(arr[i],arr[index]);
+                index++;
+            }
+         }
+    }
+};
