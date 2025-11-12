53ffcad2486a5725b24873e7ddf36035cd7c77e2
Siddhant <sidpawbake15@gmail.com>
2025-11-12 12:31:44 +0530

    Added one more approach

--- #167_TwoSum2.cpp
+++ #167_TwoSum2.cpp
@@ -26,3 +26,15 @@ public:
 };
 
 ```
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l=0,r=numbers.size()-1;
+        while(l<r){
+            if(target>numbers[l]+numbers[r]) l++;
+            if(target<numbers[l]+numbers[r]) r--;
+            if(target==numbers[l]+numbers[r]) return {l+1,r+1};
+        }
+            return{};
+    }
+};
