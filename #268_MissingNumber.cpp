d5bfcf877ab7093cb2fae99ca52c7f1208d70355
Siddhant <sidpawbake15@gmail.com>
2025-11-14 00:04:02 +0530

    New Approch Added!

--- #268_MissingNumber.cpp
+++ #268_MissingNumber.cpp
@@ -29,3 +29,13 @@ public:
 };
 
 ```
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i) return i;
+        }
+        return nums.size();
+    }
+};
