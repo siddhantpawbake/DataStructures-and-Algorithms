b9948e6dce505c10566122ff530638be743a27c1
Siddhant <sidpawbake15@gmail.com>
2025-11-16 12:43:55 +0530

    Added new Approach!

--- #75_SortColors.cpp
+++ #75_SortColors.cpp
@@ -31,3 +31,19 @@ public:
 }
 };
 ```
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int size=nums.size()-1;
+        int low=0,high=size,mid=0;
+        while(mid<=high){
+            if (nums[mid] == 0) {
+                swap(nums[low++], nums[mid++]);
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high--]);
+            }
+        }
+    }
+};
